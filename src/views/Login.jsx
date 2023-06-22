7b18475a6639140f2e2a8098069123f29ed9328d
Rodrigo Augusto Vizcarra <rodrigovizcarra9623@gmail.com>
2023-06-22 03:08:09 -0300

    Validacion de formularios completa

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -31,7 +31,7 @@ const login = () => {
                                     required: "El email es obligatorio",
                                     pattern:{
                                       value: /^(?!\.)(([^<>()\[\]\\.,;:\s@\"]+(\.[^<>()\[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                                      message: "El email es un dato obligatorio"
+                                      message: "El email debe tener el siguiente formato mail@dominio.como"
                                     }
                                 })}
                             />
--- src/views/Receta/EditarReceta.jsx
+++ src/views/Receta/EditarReceta.jsx
@@ -1,23 +1,60 @@
 import { Form, Button, FloatingLabel } from "react-bootstrap";
-
+import {useForm} from "react-hook-form";
 const EditarReceta = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+    } = useForm();
+
+    const onSubmit = (receta) => {
+        console.log(receta);
+    };
     return (
         <section className="container mainSection my-4">
             <h1 className="text-center">Editar Receta</h1>
 
             <div>
-                <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form onSubmit={handleSubmit(onSubmit)}>
+                    <Form.Group className="mb-3" controlId="inputNombreReceta">
                         <Form.Control
                             type="text"
                             placeholder="Nombre de la receta"
+                            {...register("nombreReceta", {
+                                required:
+                                    "El nombre de la receta es obligatorio",
+                                minLength: {
+                                    value: 2,
+                                    message:
+                                        "El nombre de la receta debe tener mas de dos caracteres",
+                                },
+                                maxLength: {
+                                    value: 20,
+                                    message:
+                                        "El nombre de la receta es hasta 100 caracteres",
+                                },
+                            })}
                         />
+                        <Form.Text className="text-danger">
+                            {errors.nombreReceta?.message}
+                        </Form.Text>
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Group className="mb-3" controlId="inputImagenReceta">
                         <Form.Control
                             type="text"
                             placeholder="imagen de la receta"
+                            {...register("urlProducto", {
+                                required: "La URL del producto es obligatoria",
+                                pattern: {
+                                    value: /^https:\/\/[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)*\.[a-zA-Z]{2,6}(\/[^\s]*)?$/,
+                                    message: "URL invalia",
+                                },
+                            })}
                         />
+                        <Form.Text className="text-danger">
+                            {errors.urlProducto?.message}
+                        </Form.Text>
                     </Form.Group>
                     <FloatingLabel
                         controlId="floatingTextarea2"
@@ -26,7 +63,18 @@ const EditarReceta = () => {
                         <Form.Control
                             as="textarea"
                             style={{ height: "150px" }}
+                            {...register("descripcionReceta", {
+                                required: "La descripcion es obligatoria",
+                                minLength: {
+                                    value: 150,
+                                    message:
+                                        "Como minimo una receta debe tener 150 caracteres",
+                                },
+                            })}
                         />
+                        <Form.Text className="text-danger">
+                            {errors.descripcionReceta?.message}
+                        </Form.Text>
                     </FloatingLabel>
                     <Button variant="primary" type="submit" className="mt-3">
                         Enviar
